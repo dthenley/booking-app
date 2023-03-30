89a29bfaa7ce549666305a5054cd17a899df344c
Donte <3503886+dthenley@users.noreply.github.com>
2023-03-30 16:10:09 -0500

    Setting up first page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './css/App.css';
 
-function App() {
+const services = [
+  {
+    id: 0,
+    name: 'Locs',
+    price: 80
+  },
+  {
+    id: 1,
+    name: 'Large Box Braids',
+    price: 90
+  },
+  {
+    id: 2,
+    name: 'Small Box Braids',
+    price: 70
+  },
+  {
+    id: 3,
+    name: 'Cornrolls',
+    price: 45
+  },
+  {
+    id: 4,
+    name: 'Crochet',
+    price: 45
+  }
+  ,{
+    id: 5,
+    name: 'Male Braids',
+    price: 50
+  }
+];
+
+
+const serviceList = services.map(service => 
+    <li key={service.id}>
+        <ServiceCard
+          id={service.id}
+          name={service.name}
+          price={service.price}
+        />
+    </li>
+    );
+
+export default function App() {
+const [userServices, setUserServices] = useState([]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        Booking App
+      </h1>
+      <aside>
+        <h2>Services</h2>
+
+      </aside>
+      <div className='service-option'>
+        <h2>Pick Your Service</h2>
+        <ul>
+          {serviceList}
+        </ul>
+      </div>
     </div>
   );
 }
 
-export default App;
+function ServiceCard({id, name, price}) {
+  const handleClick = () => {
+    console.log('clicked');
+  }
+  return(
+    <figure
+        onClick={handleClick}
+        name={name}
+        test="donte"
+        >
+          <img
+            src="https://via.placeholder.com/400x200"
+            alt="Book service {service.name}"
+          />
+          <figcaption>
+            <span>
+              {name}
+            </span>
+            <span>
+              ${price}
+            </span>
+          </figcaption>
+        </figure>
+  )
+}
